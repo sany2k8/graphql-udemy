4b544aeeb0fa024fa3113cea6d16b8db65708e0f
sany <sany2k8@gmail.com>
2017-08-26 22:04:43 +0600

    Fix schema to circular reference between users and company

--- schema/schema.js
+++ schema/schema.js
@@ -4,17 +4,39 @@ const {
     GraphQLObjectType,
     GraphQLInt,
     GraphQLString,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = graphql;
 
-
+const CompanyType = new GraphQLObjectType({
+   name: "Company",
+   fields: ()=> ({
+       id: { type: GraphQLString},
+       name: { type: GraphQLString},
+       description: { type: GraphQLString},
+       users:{
+           type: new GraphQLList(UserType),
+           resolve(parentValue, args){
+               return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                   .then(res => res.data)
+           }
+       }
+   })
+});
 const UserType = new GraphQLObjectType({
    name: "User",
-   fields:{
+   fields: () => ({
        id:{ type: GraphQLString},
        firstName:{ type: GraphQLString},
-       age:{ type: GraphQLInt}
-   }
+       age:{ type: GraphQLInt},
+       company :{
+           type: CompanyType,
+           resolve(parentValue, args){
+               return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                   .then(res => res.data)
+           }
+       }
+   })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -26,6 +48,14 @@ const RootQuery = new GraphQLObjectType({
            resolve(parentValue, args){
                 return axios.get(`http://localhost:3000/users/${args.id}`)
                     .then(res => res.data)
+           }
+       },
+       company: {
+           type: CompanyType,
+           args: { id: { type: GraphQLString}},
+           resolve(parentValue, args){
+               return axios.get(`http://localhost:3000/companies/${args.id}`)
+                   .then(res => res.data)
            }
        }
    }
