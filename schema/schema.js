a909b5430768bceadda400fbcca8a3e103bab86b
sany <sany2k8@gmail.com>
2017-08-27 16:21:18 +0600

    Add mutation for Company Type

--- schema/schema.js
+++ schema/schema.js
@@ -99,6 +99,39 @@ const Mutation = new GraphQLObjectType({
                 return axios.put(`http://localhost:3000/users/${args.id}`, args)
                     .then(res => res.data)
             }
+        },
+        addCompany:{
+            type: CompanyType,
+            args:{
+                name: { type: new GraphQLNonNull(GraphQLString)},
+                description: { type: GraphQLString},
+            },
+            resolve(parentValue, {name, description}){
+                return axios.post('http://localhost:3000/companies', {name, description})
+                    .then(res => res.data)
+            }
+        },
+        deleteCompany:{
+            type: CompanyType,
+            args:{
+                id: { type: new GraphQLNonNull(GraphQLString)},
+            },
+            resolve(parentValue, args){
+                return axios.delete(`http://localhost:3000/companies/${args.id}`)
+                    .then(res => res.data)
+            }
+        },
+        editCompany:{
+            type: CompanyType,
+            args:{
+                id: { type: new GraphQLNonNull(GraphQLString)},
+                name: { type: GraphQLString},
+                description: { type: GraphQLString},
+            },
+            resolve(parentValue, args){
+                return axios.put(`http://localhost:3000/companies/${args.id}`, args)
+                    .then(res => res.data)
+            }
         }
     }
 });
